0dc96425b2ee24f89fbe691b402e1e6ab8a4c4ed
John Owen <johnowen77777@gmail.com>
2023-09-06 04:48:51 +0700

    Membuat app untuk run golang-dasar module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/johnowen04/golang-dasar-app
+
+go 1.21.0
+
+require github.com/johnowen04/golang-dasar v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	golang_dasar "github.com/johnowen04/golang-dasar"
+)
+
+func main() {
+	fmt.Println(golang_dasar.SayHello())
+	fmt.Println(golang_dasar.SayHelloWorld())
+}
